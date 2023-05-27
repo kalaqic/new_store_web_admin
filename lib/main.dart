860db05667e484c6f01130b8d20df9e5a1031f59
davidkalabic <davidkalabic777@gmail.com>
2023-05-27 17:08:19 +0200

    everything working with uploading on firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_store_web/views/screens/main_scr.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyBtD6Mmytx3FdDqrOjBkL3Ag_rRGTjx0P4",
+          authDomain: "teststore-bb8d1.firebaseapp.com",
+          projectId: "teststore-bb8d1",
+          storageBucket: "teststore-bb8d1.appspot.com",
+          messagingSenderId: "229197922539",
+          appId: "1:229197922539:web:f1445d6f758be370fafeae"));
+
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MainScreen(),
--- lib/views/screens/upload_banners.dart
+++ lib/views/screens/upload_banners.dart
@@ -1,8 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
-class UploadBannerScreen extends StatelessWidget {
+class UploadBannerScreen extends StatefulWidget {
   const UploadBannerScreen({super.key});
   static const String routeName = '/UploadBannerScreen';
+
+  @override
+  State<UploadBannerScreen> createState() => _UploadBannerScreenState();
+}
+
+final FirebaseStorage _storage = FirebaseStorage.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+dynamic _image;
+String? fileName;
+
+class _UploadBannerScreenState extends State<UploadBannerScreen> {
+  void pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+    );
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+        fileName = result.files.first.name;
+      });
+    }
+  }
+
+  _uploadBannersToStorage(dynamic image) async {
+    Reference ref = _storage.ref().child('banners').child(fileName!);
+    UploadTask uploadTask = ref.putData(image);
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadURL = await snapshot.ref.getDownloadURL();
+    return downloadURL;
+  }
+
+  _uploadToFireStore() async {
+    if (_image != null) {
+      String imageURL = await _uploadBannersToStorage(_image);
+      await _firestore
+          .collection('banners')
+          .doc(fileName)
+          .set({'image': imageURL});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,27 +67,46 @@ class UploadBannerScreen extends StatelessWidget {
           color: Colors.grey[900],
           thickness: 1,
         ),
-        Row(
+        Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.grey[600],
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(10),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[600],
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: _image != null
+                        ? Image.memory(
+                            _image,
+                            fit: BoxFit.contain,
+                          )
+                        : const Center(
+                            child: Text(
+                                style: TextStyle(
+                                  fontSize: 10,
+                                ),
+                                'Upload Banner! 👇'),
+                          ),
+                  ),
                 ),
-                child: const Center(
-                  child: Text(
-                      style: TextStyle(
-                        fontSize: 10,
-                      ),
-                      'Upload Banner! 👇'),
-                ),
-              ),
-            )
+                ElevatedButton(
+                    onPressed: () {
+                      _uploadToFireStore();
+                    },
+                    child: const Text('Save!'))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  pickImage();
+                },
+                child: const Text('Upload!'))
           ],
         ),
       ],
